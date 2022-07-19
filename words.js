b15007264579b7ea3a23835dc389fe4eac5d6e2c
Jean Mayer <jean.mayer@ae.studio>
2022-07-19 09:20:13 -0300

    Adding words file

---
+++ words.js
@@ -0,0 +1,62 @@
+export const words = [
+  {
+    english: {
+      word: "Absence",
+      definition: "the state of being away from a place or person",
+      language: "en-US",
+    },
+    portuguese: {
+      word: "Ausência",
+      definition: "o estado de estar longe de um lugar ou pessoa",
+      language: "pt-BR",
+    },
+  },
+  {
+    english: {
+      word: "Justify",
+      definition: "show or prove to be right or reasonable",
+      language: "en-US",
+    },
+    portuguese: {
+      word: "Justificar",
+      definition: "mostrar ou provar estar certo ou com razão",
+      language: "pt-BR",
+    },
+  },
+  {
+    english: {
+      word: "Academic",
+      definition: "relating to education and scholarship",
+      language: "en-US",
+    },
+    portuguese: {
+      word: "Acadêmico",
+      definition: "relacionado à educação e estudos",
+      language: "pt-BR",
+    },
+  },
+  {
+    english: {
+      word: "Kitchen",
+      definition: "a room or area where food is prepared and cooked",
+      language: "en-US",
+    },
+    portuguese: {
+      word: "Cozinha",
+      definition: "uma sala ou área onde a comida é preparada e cozida",
+      language: "pt-BR",
+    },
+  },
+  {
+    english: {
+      word: "Accompany",
+      definition: "go somewhere with someone as a companion or escort",
+      language: "en-US",
+    },
+    portuguese: {
+      word: "Acompanhar",
+      definition: "ir a algum lugar com alguém como acompanhante ou escolta",
+      language: "pt-BR",
+    },
+  },
+];
